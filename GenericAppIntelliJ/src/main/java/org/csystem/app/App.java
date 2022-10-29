4bf324590d56b2565da887bb4cd16ee7041072ff
MuammerBayar <80854016+MuammerBayar@users.noreply.github.com>
2022-10-29 16:27:38 +0300

    update

--- GenericAppIntelliJ/src/main/java/org/csystem/app/App.java
+++ GenericAppIntelliJ/src/main/java/org/csystem/app/App.java
@@ -1,8 +1,25 @@
 package org.csystem.app;
 
+import org.csystem.util.console.Console;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class App {
     public static void main(String[] args)
     {
-        System.out.println("Hello, World!");
+      var prompt = Console.read("Mesajı giriniz:");
+      var formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy kk:mm:ss");
+
+      new Timer().scheduleAtFixedRate(new TimerTask() {
+
+          public void run() {
+              Console.write("%s:%s\r",prompt,formatter.format(LocalDateTime.now()));
+          }
+      },0,1000);
+
+      Console.writeLine("main ends!..");
     }
 }
--- Sample/src/main/java/org/csystem/app/App.java
+++ Sample/src/main/java/org/csystem/app/App.java
@@ -1,12 +1,31 @@
 /*----------------------------------------------------------------------------------------------------------------------
-
+    18. ders izlenmeden önce tekrar yap.
 ----------------------------------------------------------------------------------------------------------------------*/
 package org.csystem.app;
 
 import org.csystem.util.console.Console;
+
 class App {
     public static void main(String[] args)
     {
+         Sample s1 = new Sample(){
 
+         };
+
+         var s2 = new Sample() {
+
+         };
+
+         var s3 = new Sample(){
+
+         };
+
+         Console.writeLine(s1.getClass().getName());
+         Console.writeLine(s2.getClass().getName());
+         Console.writeLine(s3.getClass().getName());
     }
+}
+
+class Sample {
+
 }
