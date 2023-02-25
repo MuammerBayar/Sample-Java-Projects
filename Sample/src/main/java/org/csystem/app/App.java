bd4e3aea3bc734509bd6e4ad668140d3160dfb87
MuammerBayar <80854016+MuammerBayar@users.noreply.github.com>
2023-02-25 14:10:34 +0300

    update

--- Sample/src/main/java/org/csystem/app/App.java
+++ Sample/src/main/java/org/csystem/app/App.java
@@ -1,31 +1,8 @@
-/*----------------------------------------------------------------------------------------------------------------------
-    18. ders izlenmeden önce tekrar yap.
-----------------------------------------------------------------------------------------------------------------------*/
 package org.csystem.app;
 
-import org.csystem.util.console.Console;
+public class App {
+    public static void main(String[] args) {
 
-class App {
-    public static void main(String[] args)
-    {
-         Sample s1 = new Sample(){
-
-         };
-
-         var s2 = new Sample() {
-
-         };
-
-         var s3 = new Sample(){
-
-         };
-
-         Console.writeLine(s1.getClass().getName());
-         Console.writeLine(s2.getClass().getName());
-         Console.writeLine(s3.getClass().getName());
     }
-}
-
-class Sample {
 
 }
