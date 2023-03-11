159fb1761cd470bb7b1570508a3e5aebe26ffe16
MuammerBayar <80854016+MuammerBayar@users.noreply.github.com>
2023-03-11 23:59:16 +0300

    update

--- SampleBasic/src/samples/Main.java
+++ SampleBasic/src/samples/Main.java
@@ -7,6 +7,35 @@ package samples;
 public class Main {
     public static void main(String [] args)
     {
+        SumFactorsTest.run();
+    }
+}
 
+class SumFactorsTest {
+    public static void run()
+    {
+        java.util.Scanner kb = new java.util.Scanner(System.in);
+
+        for (;;) {
+            System.out.print("bir sayı giriniz: ");
+            int val = Integer.parseInt(kb.nextLine());
+
+            if (val == 0)
+                break;
+            System.out.printf("%d sayısının  Çarpanları toplamı: %d%n", val, NumberUtil.sumFactors(val));
+        }
+    }
+}
+
+class NumberUtil {
+    public static int sumFactors(int val)
+    {
+        int sum = 1;
+
+        for (int i = 2; i <= val / 2; ++i)
+            if (val % i == 0)
+                sum += i;
+
+        return sum;
     }
 }
