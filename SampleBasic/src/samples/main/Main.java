365c2ce763cc21ea7b6804dd0c9e01d1b9bbeaea
MuammerBayar <80854016+MuammerBayar@users.noreply.github.com>
2023-03-28 14:40:45 +0300

    update

--- SampleBasic/src/samples/main/Main.java
+++ SampleBasic/src/samples/main/Main.java
@@ -4,43 +4,9 @@
 ----------------------------------------------------------------------------------------------------------------------*/
 package samples.main;
 
-import java.util.Random;
-import java.util.Scanner;
-
-import static util.array.ArrayUtil.*;
-
 public class Main {
     public static void main(String [] args)
     {
-        SelectionSortTest.run();
-    }
-}
 
-class SelectionSortTest {
-    public static void run()
-    {
-        Random r = new Random();
-        Scanner kb = new Scanner(System.in);
-
-        for (;;) {
-            System.out.print("Dizinin eleman sayısını giriniz:");
-            int n = Integer.parseInt(kb.nextLine());
-
-            if (n <= 0)
-                break;
-
-            int [] a = getRandomArray(r, n, 0, 99);
-
-            display(2, a);
-
-            boolean desc = r.nextBoolean();
-
-            selectionSort(a, desc);
-
-            System.out.printf("%s sıralanmış dizi:%n", desc ? "Büyükten küçüğe" : "Küçükten büyüğe");
-            display(2, a);
-        }
-
-        System.out.println("Tekrar yapıyor musunuz?");
     }
 }
