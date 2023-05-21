e84178e07de9eb89a0e3e4e9d15c7566055ef79c
Muammer Bayar <muammer.bayar@ogr.iuc.edu.tr>
2023-05-21 15:57:53 +0300

    update

--- SampleBasic/src/byr/samples/game/cardgame/Card.java
+++ SampleBasic/src/byr/samples/game/cardgame/Card.java
@@ -1,14 +1,14 @@
 /*----------------------------------------------------------------------------------------------------------------------
     Card Sınıfı
-    todo: iskeletdir, yapılacaktır.
 ----------------------------------------------------------------------------------------------------------------------*/
 package byr.samples.game.cardgame;
+
+import java.util.Random;
 
 public class Card
 {
    private CardValue m_value;
    private CardType m_type;
-   //...
 
    private static void fillDeck(Card [] deck)
    {
@@ -19,15 +19,42 @@ public class Card
             deck[i++] = new Card(value, type);
    }
 
+   private static void swap(Card [] cards, int i, int k)
+   {
+      Card temp = cards[i];
+      cards[i] = cards[k];
+      cards[k] = temp;
+   }
+
+   private static void shuffledDeck(Card [] deck)
+   {
+      boolean [] numbers = new boolean[52];
+      Random r = new Random();
+
+      for (int i = 0 ; i < 52; ++i) {
+         int j = r.nextInt(52);
+         int k = r.nextInt(52);
+
+         if (!numbers[j] && !numbers[k]) {
+            swap(deck, j, k);
+            numbers[j] = true;
+            numbers[k] = true;
+         }
+      }
+   }
    public static Card [] getShuffledDeck()
    {
       Card [] deck = new Card[52];
 
       fillDeck(deck);
-
-      //...
+      shuffledDeck(deck);
 
       return deck;
+   }
+
+   public Card(String cardName)
+   {
+      setName(cardName.trim());
    }
 
    public Card(CardValue value, CardType type)
@@ -37,10 +64,26 @@ public class Card
       m_type = type;
    }
 
+   public String getName()
+   {
+      return m_value + "-" + m_type;
+   }
+
+   public void setName(String cardName)
+   {
+      String [] cardInfos = cardName.split("[-]+");
+
+      if (cardInfos.length < 2 ) {
+         System.out.println("Wrong Card Name");
+         System.exit(0);
+      }
+      m_value = CardValue.valueOf(cardInfos[0].toUpperCase());
+      m_type = CardType.valueOf(cardInfos[1].toUpperCase());
+
+   }
    public String toString()
    {
       return String.format("%s-%s", m_type.toString(), m_value.toString());
    }
 
-   //...
 }
